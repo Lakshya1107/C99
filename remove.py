d9bae2554b5cda51171615b3a0e196db8348f370
Lakshya1107 <72209696+Lakshya1107@users.noreply.github.com>
2021-11-14 16:54:18 +0530

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+dir_path = input('Enter folder path: ')
+
+treshold = time.time() - 30*86400
+for i in os.listdir(dir_path):
+    try:
+        if os.path.getmtime(dir_path + '\\' + i) < treshold:
+            os.remove(dir_path + '\\' + i)
+            print(i + ' deleted.')
+    except:
+        pass
